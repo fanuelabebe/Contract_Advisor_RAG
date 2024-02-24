02e5d3a8bcc86d45fbed21442b9abf8e5979618d
fanuelabebe <fanuelabebe@gmail.com>
2024-02-24 13:58:19 +0300

    Feat: added db history select

--- AndroidClient/app/src/main/java/com/fan/androidclient/greendao/implementation/Select.java
+++ AndroidClient/app/src/main/java/com/fan/androidclient/greendao/implementation/Select.java
@@ -1,4 +1,42 @@
 package com.fan.androidclient.greendao.implementation;
 
+import android.content.Context;
+
+import com.fan.androidclient.greendao.models.History;
+import com.fan.androidclient.greendao.models.HistoryDao;
+
+import java.util.List;
+
 public class Select {
+    public static History fetchHistoryWithId(Context context, long id){
+        HistoryDao historyDao = Common.getHistoryDao(context);
+        if (historyDao != null) {
+            try {
+                History history = historyDao.queryBuilder().where(HistoryDao.Properties.Id.eq(id)).unique();
+                if(history != null){
+                    return history;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+    public static List<History> fetchStoryList(Context context){
+        HistoryDao historyDao = Common.getHistoryDao(context);
+        if (historyDao != null) {
+            try {
+                List<History> historyList = historyDao.loadAll();
+                if(historyList != null){
+                    return historyList;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
 }
